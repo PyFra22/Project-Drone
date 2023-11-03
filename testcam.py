b1132f7e2db4d933087b2c72353627f4fd47e5e1
PyFra22 <locchifrancescoo@gmail.com>
2023-11-03 15:31:57 +0100

    Versione 1.1
    Aggiunta la possibilitá di individuare visi all'interno della schermata della fotocamera.

--- testcam.py
+++ testcam.py
@@ -4,9 +4,22 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+body_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_fulbody.xml")
+
+
 while True:
     
     _, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    facePart = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    for(x, y, width, height) in facePart:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 0), 3)
+    
     
     cv2.imshow("Camera", frame)
     
